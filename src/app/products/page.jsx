34621a0ef2f9ea590a1dd305799a06a5e89b4a11
vivek chaturvedi <vc8bp3@gmail.com>
2025-03-12 09:36:57 +0530

    added sitemap and robots.txt

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,7 +2,7 @@ import ProductPage from "./Products";
 
 export const metadata = {
   title: "Products | R J Enterprise",
-  description: "R J Enterprise – Your Trusted Partner in Steel Trading for Over 20 Years",
+  description: "R J Enterprise - Your Trusted Partner in Steel Trading for Over 20 Years",
 };
 
 export default async function page() {
